4f9235f50c543dd1d003f9dad0513771f5257089
Marek Połom <marekpolom22@gmail.com>
2021-01-24 23:25:19 +0100

    zadanie 1

--- features/steps/fizzbuzz.py
+++ features/steps/fizzbuzz.py
@@ -11,11 +11,9 @@ def step_impl(context):
 
 @when('I input (?P<number>.+)')
 def step_impl(context, number):
-    print(number)
     context.result = context.fizzbuzz.game(number)
 
 
 @then('I get result (?P<result>.+)')
 def step_impl(context, result):
-    print(result)
     assert_that(context.result).is_equal_to(result)
